f04d816fb88e39677272cffeeda7656e5cbca00e
playfulagency-dev <josemiguel@playfulagency.com>
2026-08-27 22:20:15 +0200

    fix: noindex/404 basura apex (podcast, blog pages, gracias)

--- app/podcast/layout.tsx
+++ app/podcast/layout.tsx
@@ -6,6 +6,7 @@ const PODCAST_URL = canonicalForPath('/podcast');
 export const metadata: Metadata = {
   alternates: { canonical: PODCAST_URL },
   openGraph: { url: PODCAST_URL },
+  robots: { index: false, follow: true },
 };
 
 export default function PodcastLayout({ children }: { children: React.ReactNode }) {
